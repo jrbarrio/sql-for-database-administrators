e7138f6908bcb5f4db1bd0bd1936d813f320b557
Jorge Roldan <jorge.roldan@gmail.com>
2024-05-24 21:16:15 +0200

    Introduction to relational databases

--- 1-introduction-to-relational-databases-in-sql/sql/1.2-query-information-schema.sql
+++ 1-introduction-to-relational-databases-in-sql/sql/1.2-query-information-schema.sql
@@ -1,5 +1,11 @@
-SELECT * 
-FROM information_schema.tables;
+-- Query the right table in information_schema
+SELECT table_name 
+FROM information_schema.tables
+-- Specify the correct table_schema value
+WHERE table_schema = 'public';
 
-SELECT * 
-FROM information_schema.columns;
+-- Query the right table in information_schema to get columns
+SELECT column_name, data_type 
+FROM information_schema.columns 
+WHERE table_name = 'university_professors' AND table_schema = 'public';
+
