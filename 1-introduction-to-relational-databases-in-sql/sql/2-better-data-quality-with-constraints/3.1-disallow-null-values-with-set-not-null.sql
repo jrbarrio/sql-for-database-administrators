0723af749cc62a3534425d66031b586b08adb95a
Jorge Roldan <jorge.roldan@gmail.com>
2024-05-26 18:21:21 +0200

    The not null and unique constraint

---
+++ 1-introduction-to-relational-databases-in-sql/sql/2-better-data-quality-with-constraints/3.1-disallow-null-values-with-set-not-null.sql
@@ -0,0 +1,8 @@
+-- Disallow NULL values in firstname
+ALTER TABLE professors 
+ALTER COLUMN firstname SET NOT NULL;
+
+-- Disallow NULL values in lastname
+ALTER TABLE professors
+ALTER COLUMN lastname
+SET NOT NULL;
