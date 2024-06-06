ce981301bd2d024b4d4b25da8dc1c4d78fafd2f1
Jorge Roldan <jorge.roldan@gmail.com>
2024-06-06 19:52:47 +0200

    Creating tables

---
+++ 3-creating-postgresql-databases/sql/2.1-name-that-table.sql
@@ -0,0 +1,11 @@
+-- Delete me and command below if this table name is invalid
+--CREATE TABLE 2for1 ();
+
+-- Delete me and command below if this table name is invalid
+CREATE TABLE customers ();
+
+-- Delete me and command below if this table name is invalid
+CREATE TABLE orders ();
+
+-- Delete me and command below if this table name is invalid
+CREATE TABLE service_rendered_by_other_provider ();
