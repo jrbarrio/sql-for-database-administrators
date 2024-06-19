5e9f1d97fdbb029cc75df31f6d1a773bbb189127
Jorge Roldan <jorge.roldan@gmail.com>
2024-06-19 19:47:56 +0200

    PostgreSQL access provileges

---
+++ 3-creating-postgresql-databases/sql/4-introduction-to-access-control/2.1-granting-user-privileges.sql
@@ -0,0 +1,11 @@
+-- Grant the INSERT privilege
+GRANT INSERT ON loan TO sgold;
+
+-- Grant the UPDATE privilege
+GRANT UPDATE ON loan TO sgold;
+
+-- Grant the SELECT privilege
+GRANT SELECT ON loan TO sgold;
+
+-- Grant the DELETE privilege
+GRANT DELETE ON loan TO sgold;
